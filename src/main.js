27cd467596525783cf1ea71cd54866ad6b5ad5fc
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-09-26 12:54:37 +0700

    initial commit with bootstrap-vue

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import Bootstrap from 'bootstrap-vue';
+import App from './App';
+import router from './routes';
+import AxiosPlugin from './global/axios-global';
+
+/* Bootstrap style */
+import 'bootstrap/dist/css/bootstrap.css'; /* eslint-disable-line */
+import 'bootstrap-vue/dist/bootstrap-vue.css'; /* eslint-disable-line */
+
+Vue.config.productionTip = false;
+
+/* Use the plugins */
+Vue.use(Bootstrap);
+Vue.use(AxiosPlugin, '$axios');
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
