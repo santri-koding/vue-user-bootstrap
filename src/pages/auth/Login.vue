f28f680f5d9e4f04f8348aaf7867499f19801459
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-09-26 13:14:04 +0700

    [vue-router] setup vue router

---
+++ src/pages/auth/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <b-alert show> Hello ini Login Menu </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+
+</script>
+
+<style>
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+/* import own component */
+import Login from '@/pages/auth/Login';
+import Home from '@/pages/content/Home';
 
 Vue.use(Router);
 
+const homePage = {
+  path: '/',
+  name: 'homepage',
+  component: Home,
+};
+
+const loginPage = {
+  path: '/login',
+  name: 'login',
+  component: Login,
+};
+
+const indexRoutes = [
+  homePage,
+  loginPage,
+];
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
+  mode: 'history',
+  routes: indexRoutes,
 });
