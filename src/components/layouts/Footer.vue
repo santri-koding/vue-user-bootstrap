5a5e61b9cfe5be1490d8fb03a8e2b2a8f66e4719
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-09-27 17:20:57 +0700

    add footer and help section

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,9 +1,62 @@
 <template>
-  <div class="jumbotron mb-0">
-    <div class="row">
-      <div class="col-md-4">Hello 1</div>
-      <div class="col-md-4">Hello 2</div>
-      <div class="col-md-4">Hello 3</div>
+  <div>
+    <div class="border-space"></div>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-3">
+          <h6>Forum Jual Beli Suara Merdeka</h6>
+          <ul class="link-footer">
+            <li><a href="">Tentang Kami</a></li>
+            <li><a href="">Karir</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Media Kit</a></li>
+            <li><a href="">Official Store</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h6>Jual dan Beli</h6>
+          <ul class="link-footer">
+            <li><a href="">Tentang Kami</a></li>
+            <li><a href="">Karir</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Media Kit</a></li>
+            <li><a href="">Official Store</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h6>Bantuan</h6>
+          <ul class="link-footer">
+            <li><a href="">Tentang Kami</a></li>
+            <li><a href="">Karir</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Media Kit</a></li>
+            <li><a href="">Official Store</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h6>Suara Merdeka</h6>
+          <ul class="link-footer">
+            <li><a href="">Tentang Kami</a></li>
+            <li><a href="">Karir</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Media Kit</a></li>
+            <li><a href="">Official Store</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <b-card class="text-left">
+            <div class="row">
+              <div class="col-4">
+                <img src="https://ecs7.tokopedia.net/img/footer/mobile-icon.png" alt="" class="img-fluid">
+              </div>
+              <div class="col-8" style="margin-left: -20px;">
+                <span class="font-13">Dapatkan Aplikasi FJB Suara Merdeka</span><br>
+                <span class="font-13"><a href="#">Android</a> - <a href="#">iOS</a></span>
+              </div>
+            </div>
+          </b-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +68,25 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+h6 {
+  font-size: 13px;
+}
+ul {
+  list-style: none;
+  padding-left: 10px;
+  font-size: 12px;
+}
+ul a {
+  margin-left: -10px;
+  color: var(--font-grey-custom);
+}
 
+ul li {
+  line-height: 22px;
+}
+.border-space {
+  border-top: 1px solid var(--grey-custom);
+  padding: 0;
+}
 </style>
